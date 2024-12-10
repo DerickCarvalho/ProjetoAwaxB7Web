a7b607b902daaef2a96192ceca99bed5662bd927
DerickCarvalho <derickjesiel96@gmail.com>
2024-12-10 18:37:14 -0300

    funcionalidade-team-slide

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
-const pointers = document.querySelectorAll('.pointer');
-var slide = document.getElementById('slide');
+// SLIDE DO BANNER
+
+const pointers = document.querySelectorAll('.pointer-banner');
+var slide = document.getElementById('slide-banner');
 
 pointers.forEach((pointer, index) => {
     pointer.addEventListener('click', () => {
@@ -9,5 +11,21 @@ pointers.forEach((pointer, index) => {
 
         pointer.classList.add('active');
         slide.style.marginLeft = `-${index * 100}vw`;
+    });
+});
+
+// SLIDE DO OUR TEAM
+
+const teamPointers = document.querySelectorAll('.pointer-team');
+var slideTeam = document.getElementById('slide-team');
+
+teamPointers.forEach((pointer, index) => {
+    pointer.addEventListener('click', () => {
+        teamPointers.forEach(pointerRemoveClass => {
+            pointerRemoveClass.classList.remove('active');
+        });
+
+        pointer.classList.add('active');
+        slideTeam.style.marginLeft = `-${index * 900}px`;
     });
 });
