4e644e62cb46ddd633802129b9c079659c5cd645
DerickCarvalho <derickjesiel96@gmail.com>
2024-12-13 13:24:36 -0300

    slide dos clientes

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,19 @@ teamPointers.forEach((pointer, index) => {
         slideTeam.style.marginLeft = `-${index * 900}px`;
     });
 });
+
+// SLIDE DOS CLIENTS
+
+const clientPointers = document.querySelectorAll('.pointer-client');
+var slideClient = document.getElementById('slide-clients');
+
+clientPointers.forEach((pointer, index) => {
+    pointer.addEventListener('click', () => {
+        clientPointers.forEach(pointerRemoveClass => {
+            pointerRemoveClass.classList.remove('active');
+        });
+
+        pointer.classList.add('active');
+        slideClient.style.marginLeft = `-${index * 100}vw`;
+    });
+});
